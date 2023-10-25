711d40d373b200ba0c458bc0adaae210fee17d99
Ronald Alexander Rivero Espindola <rrivero@ceibal.local>
2023-10-24 22:19:02 -0300

    mysql

---
+++ BD/001/003-select.sql
@@ -0,0 +1,6 @@
+SHOW TABLES;
+DESCRIBE productos;
+INSERT INTO productos (nombre, precio) VALUES ('Camiseta', 10.50);
+SELECT * FROM productos;
+SELECT nombre FROM productos;
+SELECT nombre, precio FROM productos;
---
+++ BD/001/004-insert_records.sql
@@ -0,0 +1,8 @@
+INSERT INTO productos (nombre, precio) VALUES
+('Camiseta Roja', 19.99),
+('Jeans Azules', 49.99),
+('Zapatillas Blancas', 89.99),
+('Gorra Negra', 14.99),
+('Remera Gris', 59.99);
+
+SELECT * FROM productos;
