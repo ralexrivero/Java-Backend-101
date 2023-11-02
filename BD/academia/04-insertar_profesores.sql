483153ee175e32ba5e2d9e9ad338b775161caea8
ralexrivero <ralexrivero@gmail.com>
2023-11-02 01:02:58 -0300

    forzar insercion de datos

---
+++ BD/academia/04-insertar_profesores.sql
@@ -0,0 +1,11 @@
+REPLACE INTO Profesores (id, nombre, apellido, especialidad) VALUES
+(1, 'Pedro', 'Vega', 'Quimica'),
+(2, 'Sofia', 'Morales', 'Quimica'),
+(3, 'Roberto', 'Mora', 'Quimica'),
+(4, 'Isabel', 'Morales', 'Arte'),
+(5, 'Miguel', 'Campos', 'Biologia'),
+(6, 'Andrea', 'Moreno', 'Fisica'),
+(7, 'Luis', 'Castillo', 'Fisica'),
+(8, 'Elena', 'Castillo', 'Historia'),
+(9, 'Roberto', 'Valdez', 'Filosofia'),
+(10, 'Isabel', 'Vargas', 'Historia');
---
+++ BD/academia/05-insertar_cursos.sql
@@ -0,0 +1,11 @@
+REPLACE INTO Cursos (id, nombre, descripcion, id_profesor, horario) VALUES
+(1, 'algebra', 'Curso introductorio de algebra', 1, '16:00:00'),
+(2, 'Historia Moderna', 'Estudio del siglo XX', 2, '16:00:00'),
+(3, 'Fisica Cuantica', 'Conceptos avanzados de Fisica Cuantica', 3, '18:00:00'),
+(4, 'Literatura Contemporanea', 'Analisis de literatura del siglo XXI', 4, '13:00:00'),
+(5, 'Biologia Celular', 'Estudio detallado de la celula', 5, '19:00:00'),
+(6, 'Quimica Organica', 'Bases de la Quimica Organica', 6, '16:00:00'),
+(7, 'Arte Renacentista', 'Arte del Renacimiento', 7, '08:00:00'),
+(8, 'Musica Clasica', 'Estudio de los grandes compositores', 8, '18:00:00'),
+(9, 'Geografia Mundial', 'Geografia a nivel mundial', 9, '09:00:00'),
+(10, 'Filosofia Oriental', 'Filosofia desde el oriente', 10, '16:00:00');
