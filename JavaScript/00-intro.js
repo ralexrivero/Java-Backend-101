e885f99865f871ffc2ad1ef1320bc040c520329e
ralexrivero <ralexrivero@gmail.com>
2023-11-09 02:01:22 -0300

    javascript

---
+++ JavaScript/00-intro.js
@@ -0,0 +1,26 @@
+// sintaxis basica
+// Variables, tipos de datos, operadores
+
+// variables
+// declaradas con `var`, `let` o `const`
+// no usamos `var`
+// `let` y `const` son la forma adecuada de declarar una variable
+
+let mensaje = "Hello World!"; // variable local del tipo string
+const PI = 3.1416; // variable local const que no se puede modificar
+
+// tipos de datos
+// numeros (enteros y flotantes)
+// textos (strings)
+// booleanos (true/false)
+// Undefined y null
+
+let nombre; // undefined, una variable no inicializada
+// const renta; no se puede dejar sin inicializar una constante
+
+nombre = 'Jose';
+nombre = 'Jose Maria';
+nombre = null;
+nombre = 7;
+
+console.log('Hola mundo!!!')
