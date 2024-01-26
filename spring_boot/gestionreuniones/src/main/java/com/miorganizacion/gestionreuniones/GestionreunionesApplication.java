4f13668dd2e8726c0fe0f65f8f6e07c76ecca10c
ralexrivero <ralexrivero@gmail.com>
2024-01-25 22:29:22 -0300

    spring boot

---
+++ spring_boot/gestionreuniones/src/main/java/com/miorganizacion/gestionreuniones/GestionreunionesApplication.java
@@ -0,0 +1,13 @@
+package com.miorganizacion.gestionreuniones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionreunionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionreunionesApplication.class, args);
+	}
+
+}
