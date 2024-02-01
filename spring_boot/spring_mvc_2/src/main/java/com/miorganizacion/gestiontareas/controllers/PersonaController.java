bdcc0fd43b7fa60059f765b0ceaa4a122ac190cc
ralexrivero <ralexrivero@gmail.com>
2024-02-01 19:57:00 -0300

    agregar thymeleaf templates

--- spring_boot/spring_mvc_2/src/main/java/com/miorganizacion/gestiontareas/controllers/PersonaController.java
+++ spring_boot/spring_mvc_2/src/main/java/com/miorganizacion/gestiontareas/controllers/PersonaController.java
@@ -1,5 +1,6 @@
 package com.miorganizacion.gestiontareas.controllers;
 
+import com.miorganizacion.gestiontareas.repositories.PersonaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
