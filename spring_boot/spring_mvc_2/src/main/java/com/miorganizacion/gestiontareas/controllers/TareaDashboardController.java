ed4c6aa0dbc03aed94e832089376ecc425ea06d4
ralexrivero <ralexrivero@gmail.com>
2024-02-01 22:31:28 -0300

    error de sintaxis

--- spring_boot/spring_mvc_2/src/main/java/com/miorganizacion/gestiontareas/controllers/TareaDashboardController.java
+++ spring_boot/spring_mvc_2/src/main/java/com/miorganizacion/gestiontareas/controllers/TareaDashboardController.java
@@ -5,7 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class TareDashboardController {
+public class TareaDashboardController {
     @GetMapping("/")
     public String home(Model model) {
         // codigo para agregar informacion relevante al model
